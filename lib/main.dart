0e833818c1472aa888cb8ef995a4fa4a9a0f2c60
mahaubin <mahaubin@gmail.com>
2022-05-19 03:01:17 +0300

    demarrage de projet

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Titre de l'aplication"),
+            centerTitle: true,
+          ),
+        ));
+  }
+}
